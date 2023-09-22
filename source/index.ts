df8690463bbb13879effea8ae2d8cd027c116af3
ZenTheDarkGod <takaczsoli@gmail.com>
2023-09-22 19:37:10 +0200

    preparing source folder for coverage test

--- source/index.ts
+++ source/index.ts
@@ -1,10 +1,8 @@
 
 import { testSend } from "./demo";
-import { ipcRenderer } from "electron";
 
 async function main() {
     const res = await testSend();
 }
 
-// ipcRenderer.send("debug-console", "anyad");
 main()
